df7472bd3d5193439b435862bab8031c58328f7e
Derek Jones <djamesjones4@gmail.com>
2017-06-02 10:15:45 -0600

    added git ignore

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,28 +1,26 @@
 (function() {
-  angular.module('classifieds', [])
-    .component('ads', {
-      controller: AdsController,
-      templateUrl: "../templates/ads.html"
-    })
-    // .component('newAdForm', {
-    //   controller: newAdController,
-    //   templateUrl: "../templates/newAdForm.html"
-    // })
-  AdsController.$inject = ['AdsService', 'NewAdFormService']
-  function AdsController(AdsService) {
-    const vm = this
-    vm.$onInit = function() {
-      AdsService.getAd().then(function(data) {
-        console.log(data)
-        vm.ads = data
+  'use strict'
+
+  angular.module('classifieds')
+    .config(config)
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
+
+  function config($stateProvider, $urlRouterProvider, $locationProvider){
+    $locationProvider.html5Mode(true)
+
+    $stateProvider
+      .state({
+        name: 'classifieds',
+        abstract: true,
+        url: '/',
+        component: 'ads'
       })
-    }
-  }
-  function newAdController(NewAdFormService) {
-    const vm = this
-    NewAdFormService.newAd().then(function(data) {
-      console.log(data)
-      vm.newAd = data
-    })
+      .state({
+        name: 'newAd',
+        parent: 'classifieds',
+        url: '/newAd',
+        component: 'newAdForm'
+      })
   }
 })()
